b67900cf6260758220cb35c9107fbfa3a79de67e
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-08-23 18:40:20 +0600

    1.0.0

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,5 @@
+fun multiply(a: Int, b: Int): Int {
+    return a * b;
+}
+
+fun voidFunc(): Unit = println("voidFunc()");
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    // dataTypes()
+    // conditionalStatements()
+    // multiply(5, 5); // 25
+    // voidFunc(); // voidFunc()
+    // arrData()
+    // repeatStr("seasons"); // 7 repetitions of the word seasons: seasonsseasonsseasonsseasonsseasonsseasonsseasons
+}
+
