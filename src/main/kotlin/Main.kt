0ba6a830a177e4fd1cd7e70fcf9b5291b19ebe70
Anatoly.Kulishov <Anatoly.Kulishov@waveaccess.ru>
2022-08-26 16:55:19 +0600

    1.0.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,27 @@
+import JetBrainsAcademy.TypesAndDataStructures.cinemaRoomManager
+import JetBrainsAcademy.TypesAndDataStructures.cuttingTheFraction
 import JetBrainsAcademy.TypesAndDataStructures.integersInAction
+import JetBrainsAcademy.TypesAndDataStructures.swap
 import Others.*
 import kotlin.reflect.typeOf
 
 fun main(args: Array<String>) {
-    // dataTypes() conditionalStatements() multiply(5, 5); // 25 voidFunc(); // Others.voidFunc() arrData()
-    // repeatStr("seasons"); // 7 repetitions of the word seasons: seasonsseasonsseasonsseasonsseasonsseasonsseasons
-    // val names = arrayOf("Bob", "John", "Alex")
-    // printNames(*names) // Bob John Alex
-    // loops()
-    // oop()
-    // oop2()
-    // oop3()
-    // swap(3, 5); // 5 3
-    // cinemaRoomManager();
-    // cuttingTheFraction(readln().toDouble());
-    // val arr = arrayListOf<Char>('h', 'e', 'l', 'l', 'o')
-    // reverseString(arr)
-    // integersInAction()
+    dataTypes()
+    conditionalStatements()
+    multiply(5, 5); // 25
+    voidFunc()
+    arrData ()
+    repeatStr("seasons"); // 7 repetitions of the word seasons: seasonsseasonsseasonsseasonsseasonsseasonsseasons
+    val names = arrayOf("Bob", "John", "Alex")
+    printNames(*names) // Bob John Alex
+    loops()
+    oop()
+    oop2()
+    oop3()
+    swap(3, 5); // 5 3
+    cinemaRoomManager();
+    cuttingTheFraction(readln().toDouble());
+    val arr = arrayListOf<Char>('h', 'e', 'l', 'l', 'o')
+    reverseString(arr)
+    integersInAction()
 }
